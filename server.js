dfbb847a5b1c107b1152818d06562082fc820889
ejgavin <ejgavin11@gmail.com>
2025-05-29 17:58:38 -0400

    Initial commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { path: "/W/videochat/socket.io" });
 
 app.use('/W/videochat', express.static(path.join(__dirname, 'public')));
 
